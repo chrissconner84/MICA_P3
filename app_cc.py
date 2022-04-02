c7e9735d271954f3bfcea8c4e3083031187e7d7c
chrissconner84 <chrissconner84@gmail.com>
2022-04-02 10:22:06 -0700

    Chris Update 4-2

--- app_cc.py
+++ app_cc.py
@@ -9,18 +9,21 @@ app = Flask(__name__)
 
 connection_string = f"postgresql://{db_user}:{db_password}@{endpoint}:5432/{db_name}"
 engine = create_engine(connection_string)
-# df = pd.read_sql("Select * from all_data", con=engine)
+all_df = pd.read_sql("Select * from all_data limit 10", con=engine)
 # usa_df= pd.read_sql("Select * from us_data", con=engine)
 test_dict={"key","value"}
 @app.route("/")
 def index():
-       countries=engine.execute("SELECT DISTINCT COUNTRY FROM all_data")
-       all_countries = [row.country for row in countries]
-       countries_json=jsonify({"countries":all_countries})
-       return render_template("index.html",countries=all_countries)
-   
-    
-
+        countries=engine.execute("SELECT DISTINCT COUNTRY FROM all_data")
+        cat_codes=engine.execute("SELECT DISTINCT CAT_CODES FROM all_data")
+        all_countries = [row.country for row in countries]
+        all_cat_codes = [row.cat_codes for row in cat_codes]
+        all_countries_df=pd.DataFrame(all_countries)
+        all_cat_df=pd.DataFrame(all_cat_codes)
+        rets=(all_countries_df.to_json(orient='records'))
+        ccodes=(all_cat_df.to_json(orient='records'))
+        return render_template("index.html",countries=all_countries,all_cat_codes=all_cat_codes,rets=rets,ccodes=ccodes)
+ 
 @app.route("/world_map")
 def worldmap():
         countries=engine.execute("SELECT DISTINCT COUNTRY FROM all_data")
@@ -43,13 +46,13 @@ def testing():
         return rets
 
 
-#@app.route("/api/all_data")
-#def get_all():
+@app.route("/api/all_data")
+def get_all():
     # [{},{},{}]
     # {k:v,k:v} -> [[k,v],[k,v],[k,v]]
-    #df = pd.read_sql("Select * from all_data", con=engine)
-    #res = [{k:v for k, v in row.items()} for i, row in df.iterrows()]
-    #return jsonify(response=res)
+    all_df = pd.read_sql("Select * from all_data limit 10", con=engine)
+    res = [{k:v for k, v in row.items()} for i, row in all_df.iterrows()]
+    return jsonify(response=res)
 
 #@app.route("/api/us_data")
 #def get_us():
