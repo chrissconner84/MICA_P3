2f57cbc984bd35833be275d1931d892a45933a66
Inderpreet Grang <granginderpreet@gmail.com>
2022-04-02 20:32:41 -0700

    Update scrape file

--- scrape_youtube.py
+++ scrape_youtube.py
@@ -29,7 +29,7 @@ def scrape():
 
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
-    results = soup.find_all('a', class_='ytd-video-renderer')
+    results = soup.find_all('a', class_='yt-simple-endpoint style-scope ytd-video-renderer')
     relative_video=[]
     relative_title=[]
     len(results)
@@ -45,7 +45,6 @@ def scrape():
         summary=article.get('aria-label')
         video_link="http://youtube.com"+article.get("href")
         if ((title is not None) and (summary is not None) and (article.get("href")  is not None)):
-
             titles.append(article.get("title"))
             summaries.append(article.get('aria-label'))
             video_links.append("http://youtube.com"+article.get("href"))
@@ -54,92 +53,6 @@ def scrape():
             # print(f"article.href:{article.get('href')}")
             # print(f"article.aria-label:{article.get('aria-label')}")
             # print(f"href={article.find('a',id_='video-title')}")
-    trends_dictionary.update({"trends":trends})
+        trends_dictionary.update({"trends":trends})
+    browser.quit()
     return(trends_dictionary)
-
-# url = 'https://spaceimages-mars.com'
-# browser.visit(url)
-# time.sleep(1)
-
-
-# html = browser.html
-# soup = BeautifulSoup(html, 'html.parser')
-
-
-# #find the featured image 
-# results = soup.find('img', class_='headerimage')["src"]
-
-# featured_image_title = soup.find('h1', class_='media_feature_title').text
-# featured_image_url=url+'/' + results
-
-
-
-# url = 'https://galaxyfacts-mars.com'
-# browser.visit(url)
-
-
-# html = browser.html
-# soup = BeautifulSoup(html, 'html.parser')
-
-
-
-# results = soup.find('table', class_='table-striped')
-# print(str(results)[0])
-# # string="""
-# # html_string = string + results + string
-
-# # from IPython.display import display_html
-# # display_html(html_string, raw=True)
-# Mars_facts_df = pd.read_html(str(results))[0]
-# Mars_facts_df.rename(columns={0:"Mars Facts", 1: "Values"}, inplace=True)
-# Mars_facts_html=Mars_facts_df.to_html()
-# Mars_fact_html_file='Mars_facts.html'
-# with open(Mars_fact_html_file, 'a') as f:
-#     f.write(Mars_facts_html)
-    
-# url = 'https://marshemispheres.com/'
-# browser.visit(url)
-# time.sleep(1)
-
-
-# html = browser.html
-# soup = BeautifulSoup(html, 'html.parser')
-
-
-
-# results = soup.find_all('div', class_='item')
-# relative_img=[]
-# for article in results:
-#     # Use Beautiful Soup's find() method to navigate and retrieve attributes
-#     relative_img.append(url+article.find('a')['href'])
-
-# hemisphere_image_urls = []
-# img_url=[]
-# title=[]
-
-# for link in relative_img:
-#     browser.visit(link)
-#     time.sleep(1)
-
-
-#     html = browser.html
-#     soup = BeautifulSoup(html, 'html.parser')
-
-#     string= (soup.find_all('li')[0])
-#     local=soup.find('h2', class_='title').text.rsplit(' ', 1)[0]
-#     local_url=link.rsplit('/',1)[0] + '/'+string.find("a")['href']
-#     title.append(local)
-#     img_url.append(local_url)
-#     hemisphere_image_urls.append({"title":local, "img_url":local_url})
-
-# browser.quit()
-# dictionary={}
-
-# dictionary.update({"news":news})
-# dictionary.update({"featured_image_title":featured_image_title})
-# dictionary.update({"featured_image_url":featured_image_url})
-# dictionary.update({"table_html_file":Mars_fact_html_file})
-# dictionary.update({"table_html":Mars_facts_html.strip()})
-
-# dictionary.update({"hemisphere_image_urls":hemisphere_image_urls})
-# return(dictionary)
